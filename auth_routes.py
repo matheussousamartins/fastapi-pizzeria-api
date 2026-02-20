a77798a001cd266d0fb03dcd0dcba767b190efd3
Matheus Sousa <matheusmartins0709@gmail.com>
2026-02-19 22:33:31 -0300

    feat(auth): complete user registration with schema validation, password hashing and proper HTTP error handling

--- auth_routes.py
+++ auth_routes.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from models import Users
 from dependencies import get_session
+from main import bcrypt_context
+from schemas import UserSchema
+from sqlalchemy.orm import Session
 
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
@@ -16,13 +19,14 @@ async def home():
     }
 
 @auth_router.post("/create_account")
-async def create_account(name: str, phone: int, email: str, password: str, session = Depends(get_session)):
-    user = session.query(Users).filter(Users.email == email).first()
+async def create_account(user_schema: UserSchema, session: Session = Depends(get_session)):
+    user = session.query(Users).filter(Users.email == user_schema.email).first()
     if user:
-        return {"mensagem": "já existe um usuário com esse email"}
+        raise HTTPException(status_code=400,detail="E-mail do usuário já cadastrado")
     else:
-        new_user = Users(name, phone, email, password)  
+        encrypted_password = bcrypt_context.hash(user_schema.password)
+        new_user = Users(user_schema.name, user_schema.phone, user_schema.email, encrypted_password, user_schema.active, user_schema.admin)  
         session.add(new_user)
         session.commit()
-        return {"mensagem": "usuário cadastrado com sucesso"}
+        return {"mensagem": f"usuário cadastrado com sucesso: {user_schema.email}"}
 
---
+++ schemas.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class UserSchema(BaseModel):
+    name: str
+    phone: int
+    email: str
+    password: str
+    active: Optional[bool]
+    admin: Optional[bool]
+
+    class Config:
+        from_attributes = True
+
